4181b11acabf20b723ac30f7e17b4662e17ff5d0
YashTotale <totaleyash@gmail.com>
2021-01-17 22:19:35 -0800

    Added types and updated fields

--- src/Utils/constants.ts
+++ src/Utils/constants.ts
@@ -1,6 +1,24 @@
 export const FIELDS = {
-  questions: [...Array(8)].map((x, i) => `Success Metric Question ${i + 1}`),
+  // Basic
+  name: "Project Name",
+  chapter: "Chapter",
+  semester: "Creation Semester",
   releaseDate: "(Anticipated) Delivery Date",
+
+  // Project
+  leads: "Project Leads (PM, Tech Lead, Designer)",
+  status: "Project Status",
+  members: "Team Members",
+
+  // Nonprofit
+  nonprofitName: "Nonprofit Partner Name",
+  nonprofitWebsite: "Nonprofit Partner Website",
+  nonprofitFocus: "Nonprofit Focus",
+  nonprofitContactName: "Nonprofit Point of Contact Name",
+  nonprofitContactEmail: "Nonprofit Point of Contact Email",
+
+  // Misc
+  questions: [...Array(8)].map((x, i) => `Success Metric Question ${i + 1}`),
 };
 
 export const SPREADSHEET_ID = "11O5zz8ff1GpWQrGdnmy973Wc7NoU3G_-RHoaFULa4Gk";
---
+++ src/Utils/types.ts
@@ -0,0 +1,38 @@
+export type Chapter =
+  | "Cal Poly"
+  | "Georgia Tech"
+  | "Cornell"
+  | "University of Maryland College Park"
+  | "Boston University"
+  | "University of Pennsylvania"
+  | "McGill University"
+  | "UIUC"
+  | "Univ of Tennessee"
+  | "Univ of Michigan"
+  | "Carleton College"
+  | "Drexel University"
+  | "Princeton University";
+
+export type ProjectStatus =
+  | "In Progress"
+  | "Delivered"
+  | "Abandoned by Dev Team"
+  | "Abandoned by Nonprofit"
+  | "Unknown";
+
+export type NonprofitFocus =
+  | "Environmental Justice"
+  | "Racial Justice"
+  | "Political"
+  | "Environmental Issues"
+  | "Education"
+  | "Poverty and Homelessness"
+  | "Immigration"
+  | "Gender Inequality"
+  | "Healthcare"
+  | "Animal Rights"
+  | "LGBTQ Rights"
+  | "Other"
+  | "Criminal Justice";
+
+export type WillingToInterview = "Yes" | "No" | "Unsure";
