36f5924209801a4a0c27410df691071775ebd9ba
YashTotale <totaleyash@gmail.com>
2021-01-19 18:07:51 -0800

    Ignored out/

--- out/Helpers/Airtable/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getAirtableTable = void 0;
-var get_airtable_table_1 = require("./get-airtable-table");
-Object.defineProperty(exports, "getAirtableTable", { enumerable: true, get: function () { return __importDefault(get_airtable_table_1).default; } });
--- out/Helpers/General/set-sheet-data.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var setSheetData = function () {
-    console.log("Setting");
-};
-exports.default = setSheetData;
