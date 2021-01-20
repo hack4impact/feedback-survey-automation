83057b8eee4d5c1370a57a5b05a35cc0d89f3dd8
subatuba21 <rohan6056@hotmail.com>
2021-01-19 22:30:35 -0800

    form updates to airtable on response

--- src/Helpers/AppsScript/make-google-form.ts
+++ src/Helpers/AppsScript/make-google-form.ts
@@ -3,10 +3,14 @@ import { GoogleFormData } from "../../Utils/types";
 const airtableAuth = `Bearer ${process.env.AIRTABLE_API_KEY}`;
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const makeGoogleForm = (request: any) => {
+const doPost = (request: any) => {
   const data = JSON.parse(request.postData.getDataAsString());
 
-  if (data.password === process.env.APPS_SCRIPT_PASSWORD) {
+  if (
+    data.password === process.env.APPS_SCRIPT_PASSWORD &&
+    data.projectId &&
+    data.questions
+  ) {
     const form = FormApp.create(data.projectName);
 
     //form config
@@ -22,7 +26,11 @@ const makeGoogleForm = (request: any) => {
     // setting form to call airTable update func on submit
     ScriptApp.newTrigger("updateProjectSuccessTable")
       .forForm(form)
-      .onFormSubmit();
+      .onFormSubmit()
+      .create();
+
+    // coupling form id with projectId
+    addRowToIdStore(form.getId(), data.projectId);
 
     const formData: GoogleFormData = {
       publishedUrl: form.getPublishedUrl(),
@@ -35,29 +43,38 @@ const makeGoogleForm = (request: any) => {
   }
 };
 
-function updateProjectSuccessTable(form: any) {
-  const response = form.response;
+function updateProjectSuccessTable(event: any) {
+  const response = event.response;
+  const form = event.source;
+  const formId = form.getId();
 
-  UrlFetchApp.fetch(
-    "https://api.airtable.com/v0/app0TDYnyirqeRk1T/Project%20Success%20Data",
-    {
-      method: "post",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: airtableAuth,
-      },
-      payload: JSON.stringify({
-        records: [
-          {
-            fields: {},
-          },
-        ],
-      }),
-    }
-  );
+  //deleting trigger to prevent overflow
+
+  let itemResponses = response.getItemResponses();
+
+  itemResponses = itemResponses.map((itemResponse: any) => {
+    const question = itemResponse.getItem().getTitle();
+    const object = {
+      question,
+      response: itemResponse.getResponse(),
+    };
+    return object;
+  });
+
+  const projectId = getProjectId(formId);
+  const projectData = getProjectData(projectId);
+
+  const fieldData = createAirtableRecordFields(
+    itemResponses,
+    response,
+    projectData
+  ); //event source is form
+  Logger.log(fieldData);
+
+  Logger.log(addRecordToAirTable(fieldData));
 }
 
-function getRowId(desiredFormId: string) {
+function getProjectId(desiredFormId: string) {
   const idStore = SpreadsheetApp.openById(
     "1J_uUVFv9EtI3raTddPRcoKi0Qs1bAEw_E3qSFQU4KD4"
   );
@@ -85,15 +102,70 @@ function getProjectData(projectId: string) {
       Authorization: airtableAuth,
     },
   });
-  const data = JSON.parse(res.getBlob().getDataAsString());
-  return data;
+  if (res.getResponseCode() === 200) {
+    const data = JSON.parse(res.getBlob().getDataAsString());
+    return data;
+  } else {
+    throw new Error("Air table project found.");
+  }
 }
 
-function mapResponseToAirtableRow(
-  formData: any,
+function createAirtableRecordFields(
+  questionResponsePairs: any,
+  response: any,
   projectData: Record<string, unknown>
 ) {
-  //
+  const projectFields: any = projectData.fields;
+  const feedbackDate: any = response.getTimestamp();
+
+  Logger.log(projectFields);
+
+  const body = {
+    Id: 0,
+    Project: [projectData["id"]],
+    "Person Giving Feedback": null,
+    "Feedback Date": `${
+      feedbackDate.getMonth() + 1
+    }/${feedbackDate.getDate()}/${feedbackDate.getYear()}`,
+    "Are you still using the product?": null,
+    "If you are not still using the product, when and why did you stop?": null,
+    "Have you had to ask for help from others to maintain the project? If so, what tasks needed to be done?": null,
+    "Has this solution caused/exacerbated any problems within your org? If so, please describe them.": null,
+    "Would you partner with Hack4Impact or a similar organization again in the future? Why or why not?": null,
+    "Success Rating 1": null,
+    "Success Rating 2": null,
+    "Success Rating 3": null,
+    "Success Rating 4": null,
+    "Success Rating 5": null,
+    "Success Rating 6": null,
+    "Success Rating 7": null,
+    "Success Rating 8": null,
+    "Overall, how well is the project working for you at this time?": null,
+    "Other Feedback": null,
+  };
+
+  //coupling questions that match with reponse body
+  for (const pair of questionResponsePairs) {
+    const question = pair.question;
+    const response = pair.response;
+
+    if (body[question] !== undefined) {
+      body[question] = response;
+    }
+  }
+
+  //Matching success metric fields to correct answers
+  for (let questionIndex = 1; questionIndex <= 8; questionIndex++) {
+    const successMetricQuestion =
+      projectFields[`Success Metric Question ${questionIndex}`];
+    for (const pair of questionResponsePairs) {
+      if (pair.question == successMetricQuestion) {
+        body[`Success Rating ${questionIndex}`] = parseInt(pair.response); // response has to be number
+      }
+    }
+  }
+
+  return body;
 }
 
 function addRecordToAirTable(data: any) {
