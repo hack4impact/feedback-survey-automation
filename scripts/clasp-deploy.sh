24209b77e44f435b623a3c26f5b1e326df7b2cf9
Yash Totale <totaleyash@gmail.com>
2021-01-22 23:52:44 -0800

    Updated Clasp Deploy Script

---
+++ scripts/clasp-deploy.sh
@@ -0,0 +1,9 @@
+# Current Directory
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+# Set environment vars
+source "$DIR/exporter.sh" APP_SCRIPT_ID
+
+# Deploy command
+OUTPUT=$(clasp deploy -i $APPS_SCRIPT_ID)
+echo "${OUTPUT}"
---
+++ scripts/exporter.sh
@@ -0,0 +1,11 @@
+do-export() {
+  export $(cat .env | xargs)
+}
+
+if [ $# -eq 0 ]; then
+  do-export && return
+fi
+
+for i in "$@"; do
+  if [ -z ${!i+x} ]; then do-export && return; fi
+done
