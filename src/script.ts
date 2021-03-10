37ff995ff210483625d539c3b349c5ff11a43fbc
Yash Totale <totaleyash@gmail.com>
2021-03-10 10:23:37 -0800

    Added Top-Level Try Catch

--- src/script.ts
+++ src/script.ts
@@ -22,12 +22,12 @@ import Logger from "./Helpers/Logger";
 import { FIELDS } from "./Utils/constants";
 
 process.on("unhandledRejection", (e) => {
-  console.error(e);
+  Logger.error(e);
   process.exit(1);
 });
 
 process.on("uncaughtException", (e) => {
-  console.error(e);
+  Logger.error(e);
   process.exit(1);
 });
 
@@ -41,85 +41,89 @@ const script = async () => {
   const dryRun = args["dry-run"] || process.env.DRY_RUN !== "false";
 
   const logger = new Logger(dryRun);
+  try {
+    const table = Airtable.base(process.env.AIRTABLE_BASE_ID ?? "");
 
-  const table = Airtable.base(process.env.AIRTABLE_BASE_ID ?? "");
+    const standardQuestions = await getStandardQuestions(table);
 
-  const standardQuestions = await getStandardQuestions(table);
+    table("Projects")
+      .select()
+      .eachPage(
+        async (projects, nextPage) => {
+          for (const project of projects) {
+            const data = parseProject(project);
+            const { projectName } = data;
 
-  table("Projects")
-    .select()
-    .eachPage(
-      async (projects, nextPage) => {
-        for (const project of projects) {
-          const data = parseProject(project);
-          const { projectName } = data;
+            // Checks for empty rows
+            if (typeof projectName !== "string") continue;
 
-          // Checks for empty rows
-          if (typeof projectName !== "string") continue;
+            // Log the project's name
+            Logger.line();
+            Logger.bold(projectName);
 
-          // Log the project's name
-          Logger.line();
-          Logger.bold(projectName);
+            // Make sure the project has all required fields. If not, throw an error.
+            const checkedData = checkRequiredFields(data);
 
-          // Make sure the project has all required fields. If not, throw an error.
-          const checkedData = checkRequiredFields(data);
+            // Flatten fields that are supposed to be strings (but Airtable returns an array with 1 element)
+            const flattenedData = flattenFields(checkedData);
+            const { projectStatus, projectSuccessData } = flattenedData;
 
-          // Flatten fields that are supposed to be strings (but Airtable returns an array with 1 element)
-          const flattenedData = flattenFields(checkedData);
-          const { projectStatus, projectSuccessData } = flattenedData;
+            // If project is abandoned, skip
+            if (!(await checkProjectStatus(logger, projectStatus))) continue;
 
-          // If project is abandoned, skip
-          if (!(await checkProjectStatus(logger, projectStatus))) continue;
+            const successData = await getProjectSuccessData(
+              table,
+              projectSuccessData
+            );
 
-          const successData = await getProjectSuccessData(
-            table,
-            projectSuccessData
-          );
+            // If project is not in use by nonprofit, skip
+            if (
+              !(await checkInUse(
+                successData,
+                project,
+                standardQuestions,
+                dryRun,
+                logger
+              ))
+            )
+              continue;
 
-          // If project is not in use by nonprofit, skip
-          if (
-            !(await checkInUse(
-              successData,
+            const reminderNeeded = await checkReminderNeeded(
+              flattenedData,
+              logger
+            );
+
+            // If reminder not needed, skip
+            if (reminderNeeded === null) continue;
+
+            await createGoogleForm(
               project,
-              standardQuestions,
+              flattenedData,
+              reminderNeeded,
               dryRun,
               logger
-            ))
-          )
-            continue;
+            );
+            await sendReminderEmail(
+              flattenedData,
+              reminderNeeded,
+              logger,
+              dryRun
+            );
 
-          const reminderNeeded = await checkReminderNeeded(
-            flattenedData,
-            logger
-          );
+            !dryRun &&
+              (await project.updateFields({
+                [FIELDS.lastSent]: reminderNeeded,
+              }));
+          }
 
-          // If reminder not needed, skip
-          if (reminderNeeded === null) continue;
-
-          await createGoogleForm(
-            project,
-            flattenedData,
-            reminderNeeded,
-            dryRun,
-            logger
-          );
-          await sendReminderEmail(
-            flattenedData,
-            reminderNeeded,
-            logger,
-            dryRun
-          );
-
-          !dryRun &&
-            (await project.updateFields({
-              [FIELDS.lastSent]: reminderNeeded,
-            }));
-        }
-
-        nextPage();
-      },
-      () => logger.finish()
-    );
+          nextPage();
+        },
+        () => logger.finish()
+      );
+  } catch (e) {
+    logger.error(e.message);
+    process.exit(1);
+  }
 };
 
 script();
