93fc768188dc01dff4e7a444a2361c1cb0978b0d
Yash Totale <totaleyash@gmail.com>
2021-03-13 10:26:14 -0800

    Added Better Loading Functionality on Website

--- website/index.js
+++ website/index.js
@@ -6,14 +6,72 @@ const PROD_LOGS_URL =
 
 const logIframe = document.getElementById("logIframe");
 const showDevLogs = document.getElementById("showDevLogs");
+const loadingContainer = document.getElementById("loadingContainer");
 
 showDevLogs.checked = false;
-logIframe.src = PROD_LOGS_URL;
+logIframe.setAttribute("data-type", "prod");
+logIframe.src = getIframeSrc();
+
+window.addEventListener("message", (event) => {
+  console.log("listener called!", event);
+  if (event.origin.endsWith("script.googleusercontent.com")) {
+    const { type } = event.data;
+
+    switch (type) {
+      case "dateChange": {
+        const { date, start, end } = event.data;
+
+        if (date) {
+          logIframe.src = `${getIframeSrc()}?date=${date}`;
+        } else if (start && end) {
+          logIframe.src = `${getIframeSrc()}?start=${start}&end=${end}`;
+        }
+
+        addLoading();
+        break;
+      }
+      case "showMoreLogs": {
+        const { start, end } = event.data;
+
+        if (start && end) {
+          logIframe.src = `${getIframeSrc()}?start=${start}&end=${end}`;
+        }
+        addLoading();
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  }
+});
+
+logIframe.onload = function () {
+  removeLoading();
+};
 
 showDevLogs.onclick = function () {
   if (showDevLogs.checked) {
-    logIframe.src = DEV_LOGS_URL;
+    logIframe.setAttribute("data-type", "dev");
+    logIframe.src = getIframeSrc();
   } else {
-    logIframe.src = PROD_LOGS_URL;
+    logIframe.setAttribute("data-type", "prod");
+    logIframe.src = getIframeSrc();
   }
+  addLoading();
 };
+
+function removeLoading() {
+  loadingContainer.classList.remove("d-flex");
+  loadingContainer.style.display = "none";
+}
+
+function addLoading() {
+  loadingContainer.classList.add("d-flex");
+  loadingContainer.style.display = "flex";
+}
+
+function getIframeSrc() {
+  const type = logIframe.getAttribute("data-type");
+  return type === "dev" ? DEV_LOGS_URL : PROD_LOGS_URL;
+}
